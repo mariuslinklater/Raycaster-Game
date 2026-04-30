e7e6a828d97704967d70687760623fb38a2cf6f5
marius <mariuslinklater@gmail.com>
2026-04-30 15:24:50 -0600

    most of class other work, worked on wall class

--- sketch.js
+++ sketch.js
@@ -5,28 +5,15 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-console.log('diddy')
-const TILE_SIZE = 10;
-let map = [[1,1,1,1,1]
-           [1,0,0,0,1]
-           [1,0,1,0,1]
-           [1,0,1,0,1]
-           [1,1,1,1,1]];
-
-class Ray(x, y) {
+class Ray {
   constructor(){
-    this.x = x;
-    this.y = y;
+    
+  }
+}
+class Wall {
+  constructor(){
 
   }
-}  
-
-class Wall(x, y) {
-  constructor(x, y) {
-  this.x = x;
-  this.y = y;  
-  }
-
 }
 
 function setup() {
@@ -35,14 +22,4 @@ function setup() {
 
 function draw() {
   background(220);
-}
-
-function makeFlatMap() {
-  for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === 1) {
-        square(x * TILE_SIZE,y * TILE_SIZE, TILE_SIZE);  
-      }
-    }
-  }
 }
