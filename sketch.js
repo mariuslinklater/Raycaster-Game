49cfc2129abbf48067d6d061ab21d5be8cc16bd4
marius <mariuslinklater@gmail.com>
2026-05-01 15:18:34 -0600

    quiz today, worked on player class mostly

--- sketch.js
+++ sketch.js
@@ -4,7 +4,15 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let cellSize = 20;
+let 
+let map = [[1,1,1,1,1,1,1,1],
+           [1,0,0,1,0,0,0,1],
+           [1,0,0,1,0,0,0,1],
+           [1,0,0,0,0,0,0,1],
+           [1,0,0,0,0,1,0,1],
+           [1,0,0,0,0,1,0,1],
+           [1,1,1,1,1,1,1,1]]
 class Ray {
   constructor(){
     
@@ -16,10 +24,26 @@ class Wall {
   }
 }
 
+class Player {
+  constructor(x, y, fov) {
+    this.x = x;
+    this.y = y;
+    this. viewAngle = 0;
+    this.fov = PI/4;
+  }
+  move(){
+    if(this.x)
+  }
+  rotate(){
+  
+  }
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+  for (let )
 }
