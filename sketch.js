a9a75afb517884839ae20580a9e7ac93f70b7215
marius <mariuslinklater@gmail.com>
2026-05-06 15:24:16 -0600

    worked further on player class

--- sketch.js
+++ sketch.js
@@ -5,22 +5,24 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let cellSize = 20;
-let 
-let map = [[1,1,1,1,1,1,1,1],
-           [1,0,0,1,0,0,0,1],
-           [1,0,0,1,0,0,0,1],
-           [1,0,0,0,0,0,0,1],
-           [1,0,0,0,0,1,0,1],
-           [1,0,0,0,0,1,0,1],
-           [1,1,1,1,1,1,1,1]]
+let map = 
+  [[1,1,1,1,1,1,1,1],
+    [1,0,0,1,0,0,0,1],
+    [1,0,0,1,0,0,0,1],
+    [1,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,0,1],
+    [1,0,0,0,0,1,0,1],
+    [1,1,1,1,1,1,1,1]];
 class Ray {
-  constructor(){
+  constructor(x, y, angle){
     
+  }
+  cast(){
+
   }
 }
 class Wall {
   constructor(){
-
   }
 }
 
@@ -29,10 +31,25 @@ class Player {
     this.x = x;
     this.y = y;
     this. viewAngle = 0;
-    this.fov = PI/4;
+    this.fov = 45;
+    this.viewfield = [];
+    for (let i = 0; i < this.fov; i++) {
+      this.viewfield.push(new Ray(this.x, this.y, i));
+    }
   }
   move(){
-    if(this.x)
+    if (keyIsDown(68)) {
+      this.x++;
+    }
+    if (keyIsDown(65)) {
+      this.x--; 
+    }
+    if (keyIsDown(87)) {
+      this.y++;
+    }
+    if (keyIsDown(83)) {
+      this.y--;  
+    }
   }
   rotate(){
   
@@ -45,5 +62,4 @@ function setup() {
 
 function draw() {
   background(220);
-  for (let )
 }
