5093b8be53854082df458198de0f023a5c60ef83
marius <mariuslinklater@gmail.com>
2026-05-13 15:24:53 -0600

    wrote worst code ever

--- sketch.js
+++ sketch.js
@@ -24,8 +24,15 @@ class Ray {
 }
 class Wall {
   constructor(ay, ax, by, bx){
-    this.a = createVector(ay, ax);
-    this.b = createVector(by, bx);
+    this.ay = ay;
+    this.ax = ax;
+    this.by = by;
+    this.bx = bx;
+  }
+
+  draw(a, b) {
+    stroke(255);
+    line(this.ax, this.ay, this.bx, this.by);
   }
 }
 
@@ -61,30 +68,34 @@ class Player {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(220);
   for (let x = 0; x < map.length; x++){ // loops through the map to see where the walls are, if a point on the map is in a wall it checks its neighbors to make actual walls
     for (let y = 0; x < map[x].length; y++){
       if (map[x][y] === 1){
         if (map[x][y - 1] === 1) {
-          let wall = new Wall(map[x], map[y], map[x], map[y - 1]);
+          let wall = new Wall(map[x] * cellSize, map[y] * cellSize , map[x] * cellSize, map[y - 1] * cellSize);
           walls.push(wall);
         }
         if (map[x][y + 1] === 1){
-          let wall = new Wall(map[x], map[y], map[x], map[y + 1]);
+          let wall = new Wall(map[x] * cellSize, map[y] * cellSize, map[x] * cellSize, map[y + 1] * cellSize);
           walls.push(wall);
         }
         if (map[x + 1][y] === 1){
-          let wall = new Wall(map[x], map[y], map[x + 1], map[y]);
+          let wall = new Wall(map[x] * cellSize, map[y] * cellSize, map[x + 1] * cellSize, map[y] * cellSize);
           walls.push(wall);
         }
-        if (map[x - 1][y] === 1) {
-          let wall = new Wall(map[x], map[y], map[x - 1], map[y]);
+        if (!map[x] === 0 && map[x - 1][y] === 1) {
+          console.log('john pork');
+          let wall = new Wall(map[x] * cellSize, map[y] * cellSize, map[x - 1] * cellSize, map[y] * cellSize);
           walls.push(wall);
         }
       }
     }
+  }
+}
+
+function draw() {
+  background(220);
+  for (wall of walls) {
+    wall.draw();
   }
 }
