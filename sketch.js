c95450a53cdc9c434858e4821125f942437b378b
marius <mariuslinklater@gmail.com>
2026-05-26 15:14:52 -0600

    worked on player movement and ray wall collision detection, both need more work

--- sketch.js
+++ sketch.js
@@ -5,7 +5,9 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let harjot;
+let player;
+let viewAngleValue = 0;
+let rays  = [];
 let cellSize = 20;
 let theWalls = [];
 let map = 
@@ -27,10 +29,10 @@ class Ray {
   cast(){
   }
   collide(Ray, wall) {
-    let collisionTrue = collideLineLine(this.Ray.x1, this.Ray.x2, this.wall.x1, this.wall.x2);
-    if (collisionTrue) {
-      return collideLineLine(this.Ray.x1, this.Ray.x2, this.wall.x1, this.wall.x2, true);
-    }
+    let collisionPoint = collideLineLine(Ray.x1, Ray.x2, wall.x1, wall.x2, true);
+    fill('red');
+    circle(collisionPoint.x, collisionPoint.y, 15);
+
   }
   draw() {
     let xVector = this.length * cos(this.angle);
@@ -49,7 +51,6 @@ class Wall {
   }
 
   draw() {
-    strokeWeight(5);
     stroke('black');
     line(this.ax, this.ay, this.bx, this.by);
   }
@@ -61,31 +62,57 @@ class Player {
     this.y = y;
     this. viewAngle = 0;
     this.fov = fov;
-    this.viewfield = [];
     for (let i = 0; i < this.fov; i++) {
-      this.viewfield.push(new Ray(this.x, this.y, i));
+      rays.push(new Ray(this.x, this.y, i));
     }
   }
   
-  drawHarjot(x, y) {
+  draw() {
+    if (keyIsDown(87)) {
+      player.y -= 1;
+    }
+    if (keyIsDown(83)) {
+      player.y += 1;
+    }
+    if (keyIsDown(68)) {
+      player.x += 1;
+    }
+    if (keyIsDown(65)) {
+      player.x -= 1;
+    }
     fill('red');
-    circle(x, y, 5);
+    circle(this.x, this.y, 5);
   }
   
 }
 
 function setup() {
+  angleMode(DEGREES);
   createCanvas(windowWidth, windowHeight); 
-  let harjot = new Player(width/2, height/2, 360);
   let wall = new Wall(random(0, height), random(0, width), random(0, height), random(0, width));
-  theWalls.push(wall);
+  player = new Player(random(0, width), random(0, height), 60);
+  theWalls.push(wall);;
 
 }
 
 function draw() {
   background(220);  
   drawWalls(theWalls);
-  runPlayer();harjot.drawHarjot();
+
+  player.draw();
+
+  for(let ray of rays) {
+    ray.x = player.x;
+    ray.y = player.y;
+    ray.draw();
+  }
+
+
+  for(let ray = 0; ray < rays.length; ray++){
+    for(let wall = 0; wall < theWalls.length; wall++){
+      rays[ray].collide(rays[ray], theWalls[wall]);
+    }
+  }
 }
 
 function drawWalls(theWalls) {
@@ -107,6 +134,17 @@ function makeMapWalls(map) {
   }
 }
 
-function runPlayer() {
-  drawHarjot(mouseX, mouseY);  
+function keyIsDown() {
+  if (key === w) {
+    player.y -= 1;
+  }
+  if (key === s) {
+    player.y += 1;
+  }
+  if (key === d) {
+    player.x += 1;
+  }
+  if (key === a) {
+    player.x -= 1;
+  }
 }
